6624bec63df3ee38afe90d799915415fe3df4e97
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-03 20:46:04 +0400

    Update NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
+   long ans = 0;
+        for(int i=1; i<=lengthOfLastNumber; i++){
+            long number= 9;
+            for (int j=1; j<i; j++){
+                number=(number*10)+9;
+            }
+            ans+=number;
+        }
+
+        System.out.println(ans);
 
     }
 }
