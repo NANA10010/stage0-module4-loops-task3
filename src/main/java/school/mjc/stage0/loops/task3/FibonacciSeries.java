e70a1543ab74a0255b2138fe66df4c05640c3ef6
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-03 20:53:29 +0400

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+            int ans=0;
+        int first = 0;
+        int second = 1;
+       System.out.println(first);
+       System.out.println(second);
+        for(int i=0; i<lastFibonacci-2; i++){
+            ans=first+second;
+            first = second;
+            second = ans;
+            System.out.println(ans);
+        }
+
     }
 }
