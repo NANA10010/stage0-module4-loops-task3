c3ddcde76c56af2efc72096ef24812f48656b2d1
NANA10010 <86974505+NANA10010@users.noreply.github.com>
2023-02-03 21:53:43 +0400

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,7 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
+    public void printDigitsSum(int number){
          int ans=0;
         for (int i=0; i<20; i++){
             ans+=number%10;
